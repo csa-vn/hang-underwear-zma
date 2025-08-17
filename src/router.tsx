ea4fbcd049a3400a1ed0953450790b31b2d85a7c
azazeldev <trunghoang1124@gmail.com>
2025-08-17 21:18:47 +0700

    feat: add OpenZMA page and manifest for Zalo Mini App integration

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,7 @@ import CategoryListPage from "@/pages/catalog/category-list";
 import ProductDetailPage from "@/pages/catalog/product-detail";
 import HomePage from "@/pages/home";
 import ProfilePage from "@/pages/profile";
+import OpenZMA from "@/pages/zma-link";
 import SearchPage from "@/pages/search";
 import { createBrowserRouter } from "react-router-dom";
 import { getBasePath } from "@/utils/zma";
@@ -45,6 +46,13 @@ const router = createBrowserRouter(
             logo: true,
           },
         },
+          {
+            path: "/zma-link",
+            element: <OpenZMA />,
+            handle: {
+              title: "Mở Zalo Mini App",
+            },
+          },
         {
           path: "/flash-sales",
           element: <ProductListPage />,
